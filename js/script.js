16c15bd3d4e09a2fa0c4ed2441f2bf18b459a82a
KindPixel <thomas.lima@viacesi.fr>
2021-12-06 17:24:05 +0100

    presque

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
 
+    function goto(href) {
+        window.location(href);
+    }
+
             $.ajax({
                 url: "php/listingfiles.php",
-                type: "POST",
+                type: "GET",
                 cache: false,
                 success: function(dataResult) {
                     dataResult = JSON.parse(dataResult);
                     dataResult = Object.values(dataResult);
                     $.each(dataResult, function (indexInArray, valueOfElement) { 
-                        markup = "<tr><th><a href='../document_viewer/pages/"+ valueOfElement +"'>" + indexInArray + "</th><td> " + valueOfElement + "</a></td></tr>";
+                        //markup = "<tr><th><a href='../document_viewer/pages/"+ valueOfElement +"'>" + indexInArray + "</th><td> " + valueOfElement + "</a></td></tr>";
+                        href = "../document_viewer/pages/"+ valueOfElement;
+                        
+                        markup = "<tr style='cursor: pointer;' onclick="+goto(href)+"><th>" + indexInArray + "</th><td> " + valueOfElement + "</td></tr>";
                         tableBody = $("table tbody");
                         tableBody.append(markup);
+                        
                     });
                 }
             });
