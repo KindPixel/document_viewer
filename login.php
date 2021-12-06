f9ecd527e7c4557914402b1c80b2ff0ee8df5af2
KindPixel <thomas.lima@viacesi.fr>
2021-12-06 10:10:12 +0100

    init

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php include("navbar.php"); ?>
+
+<script type="text/javascript" src="../jsScripts/loginregister.js"></script>
+
+<div class="loginbox">
+
+	<form id="login_form" method="post">
+		
+		<div class="form-group">
+			<label for="pwd">Email:</label>
+			<input type="email" class="form-control" id="email_log" placeholder="Email" name="email">
+		</div>
+		<div class="form-group">
+			<label for="pwd">Password:</label>
+			<input type="password" class="form-control" id="password_log" placeholder="Password" name="password">
+		</div>
+		<input type="button" name="save" class="btn btn-primary" value="Login" id="butlogin">
+	</form>
+
+</div>
---
+++ navbar.php
@@ -0,0 +1,10 @@
+<!-- FontAwesome -->
+<link href="../resources/fontawesome-free-5.15.4-web/css/all.css" rel="stylesheet">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- My css -->
+<link rel="stylesheet" href="../style/style.css">
+<!-- Jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
